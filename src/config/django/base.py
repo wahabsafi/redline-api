f184578d2de8a428a0d90fb276623f53e07ca8fb
wahabsafi <wahab.safi3@gmail.com>
2024-01-05 11:00:56 +0330

    adding user app to installed apps and changing auth_user_model

--- src/config/django/base.py
+++ src/config/django/base.py
@@ -30,7 +30,9 @@ ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
 
 
 # Application definition
-LOCAL_APPS=[]
+LOCAL_APPS=[
+    'redline.users.apps.UsersConfig',
+]
 THIRD_PARTY_APPS=[]
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -116,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+AUTH_USER_MODEL = "users.BaseUser"
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -140,4 +142,4 @@ APP_DOMAIN = env("APP_DOMAIN", default="http://localhost:8000")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-from config.settings.jwt import *  # noqa
+# from config.settings.jwt import *  # noqa
