5a7278098275aef1304334566bf66fafd222ffbf
wahabsafi <wahab.safi3@gmail.com>
2024-01-05 11:10:26 +0330

    init common app

---
+++ src/redline/common/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.db.models.query import F, Q
+from django.utils import timezone
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(db_index=True, default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def update(self, commit=False, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        if commit:
+            self.save()
+
+
