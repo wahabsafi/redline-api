85f14a5742345313e4fb30a5ee57b574ce8a0bc0
wahabsafi <wahab.safi3@gmail.com>
2024-02-02 20:27:22 +0330

    creating login serializer

---
+++ src/redline/authentication/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+
+class LoginSerializer(serializers.Serializer):
+    email=serializers.EmailField(required=False)
+    phone_number=serializers.CharField(max_length=14,required=False)
+    username=serializers.CharField(required=False)
+    password=serializers.CharField(required=True,style={'input_type':'password'})
