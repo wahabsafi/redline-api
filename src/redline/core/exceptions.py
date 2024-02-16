3eca24c1d7c96e2ee65cdd4b170c43794ce6a0b3
wahabsafi <wahab.safi3@gmail.com>
2024-02-16 09:52:42 +0330

    adding Response Exception class

--- src/redline/core/exceptions.py
+++ src/redline/core/exceptions.py
@@ -1,6 +1,24 @@
+from django.db.utils import Error
+
+
 class ApplicationError(Exception):
     def __init__(self, message, extra=None):
         super().__init__(message)
 
         self.message = message
         self.extra = extra or {}
+
+
+class ResponseException:
+    """
+    This class aim to remove unnecessary deatil of exception messages and provide proper message
+    """
+    def __init__(self, exception: Exception) -> None:
+        
+        self.exception = exception
+
+    def __str__(self) -> str:
+        error_message = str(self.exception)
+        if issubclass(self.exception.__class__, Error):
+            error_message = error_message[error_message.index("DETAIL") :]
+        return error_message
