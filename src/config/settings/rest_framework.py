6facea74d72583cc150d0dfc890171615ba1f84f
wahabsafi <wahab.safi3@gmail.com>
2024-02-16 09:44:24 +0330

    set DEFAULT_SCHEMA_CLASS

--- src/config/settings/rest_framework.py
+++ src/config/settings/rest_framework.py
@@ -2,6 +2,8 @@ REST_FRAMEWORK = {
     
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+        
+    ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     
 }
