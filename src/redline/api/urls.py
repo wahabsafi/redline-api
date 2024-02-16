96f324807bd05c8b92317c39cfb96ea1206bcb78
wahabsafi <wahab.safi3@gmail.com>
2024-02-16 09:45:17 +0330

    including user app urls into main urls

--- src/redline/api/urls.py
+++ src/redline/api/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import include, path
 
-app_name='api'
+app_name = "api"
 
 urlpatterns = [
-    path('auth/',include('redline.authentication.urls','auth'))
+    path("auth/", include("redline.authentication.urls", "auth")),
+    path("", include("redline.users.urls", "user")),
 ]
