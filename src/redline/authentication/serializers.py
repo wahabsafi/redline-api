e731922ecd3fc474f7d61c8714ce090739b87fc7
wahabsafi <wahab.safi3@gmail.com>
2024-02-16 09:46:01 +0330

    adding jwt_output serialzer class

--- src/redline/authentication/serializers.py
+++ src/redline/authentication/serializers.py
@@ -5,3 +5,7 @@ class LoginSerializer(serializers.Serializer):
     phone_number=serializers.CharField(max_length=14,required=False)
     username=serializers.CharField(required=False)
     password=serializers.CharField(required=True,style={'input_type':'password'})
+
+class JWTOutput(serializers.Serializer):
+    access=serializers.CharField()
+    refresh=serializers.CharField()
